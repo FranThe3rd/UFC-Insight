18bff48f3f67068edfe56fe0c513145bfbfb9cc2
Francisco <figueroaf@moravian.edu>
2024-12-25 16:54:59 -0500

    changed some things

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 sudo yum install git -y
 sudo yum install java-17-openjdk -y
 sudo yum install maven -y
+
+export DB_PASSWORD=password
+
 git clone https://github.com/FranThe3rd/UFC-Insight.git
 cd UFC-Insight
 cd backend
 mvn clean package
 mvn spring-boot:run
-
