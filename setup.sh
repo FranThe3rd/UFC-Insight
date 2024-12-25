d6322792071fa64e7749179ed7d2114ce9bc6c8e
Francisco <figueroaf@moravian.edu>
2024-12-25 16:23:07 -0500

    added new setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,9 @@
-#!/bin/bash
-
-# Update and install necessary dependencies
-echo "Updating package list and installing required packages..."
-sudo apt-get update -y
-sudo apt-get install -y git openjdk-11-jdk maven
-
-# Verify installations
-echo "Verifying Maven installation..."
-mvn -v
-echo "Verifying Java installation..."
-java -version
-
-# Clone the repository
-echo "Cloning the repository..."
+sudo yum install git -y
+sudo yum install java-17-openjdk -y
+sudo yum install maven -y
 git clone https://github.com/FranThe3rd/UFC-Insight.git
+cd UFC-Insight
+cd backend
+mvn clean package
+mvn spring-boot:run
 
-# Navigate to the backend directory
-cd UFC-Insight/backend || { echo "Backend directory not found."; exit 1; }
-
-# Build the project with Maven
-echo "Building the project with Maven..."
-./mvnw clean package || { echo "Maven build failed."; exit 1; }
-
-# Run the Spring Boot application
-echo "Running the Spring Boot application..."
-./mvnw spring-boot:run
