37307c7be48cdde49ed06fba4919b900b003a3c8
Francisco <figueroaf@moravian.edu>
2025-03-04 15:08:23 -0500

    added user

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@ sudo yum install git -y
 sudo yum install java-17-openjdk -y
 sudo yum install maven -y
 
-export DB_PASSWORD=password
+export DB_PASSWORD=ufc-database-conor-2025
 git clone https://github.com/FranThe3rd/UFC-Insight.git
 cd UFC-Insight
 cd backend
 mvn clean package
-mvn spring-boot:run
+nohup mvn spring-boot:run &
 
 # Probably should use to keep it running after closing ec2 ```nohup mvn spring-boot:run &```
